17c9afd3c17e99485655cbe49d363e5eee15090f
Jourdelune <jourdelune863@gmail.com>
2024-06-06 19:28:07 +0200

    [update] get audio links

--- Crawler/items.py
+++ Crawler/items.py
@@ -6,7 +6,5 @@
 import scrapy
 
 
-class CrawlerItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class AudioItem(scrapy.Item):
+    link = scrapy.Field()
