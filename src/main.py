d7302e907b78d3f79fe65d3eaf80e39342edb014
jourdelune <jourdelune863@gmail.com>
2024-07-23 14:33:50 +0200

    [update] add args

--- src/main.py
+++ src/main.py
@@ -8,6 +8,35 @@ from crawlee.beautifulsoup_crawler import BeautifulSoupCrawler
 
 from routes import router
 from utils import process
+import argparse
+
+parser = argparse.ArgumentParser(description="Crawl the web to find audio files")
+
+parser.add_argument(
+    "-o",
+    "--output",
+    type=str,
+    default="results.json",
+    help="output file name",
+)
+
+parser.add_argument(
+    "-m",
+    "--max_requests",
+    type=int,
+    default=1_000_000,
+    help="maximum number of requests per crawl",
+)
+
+parser.add_argument(
+    "-u",
+    "--urls",
+    type=str,
+    nargs="+",
+    default=["https://freemusicarchive.org"],
+    help="list of urls to start the crawl",
+)
+args = parser.parse_args()
 
 
 async def main() -> None:
@@ -16,12 +45,12 @@ async def main() -> None:
     """
 
     crawler = BeautifulSoupCrawler(
-        request_handler=router,
+        request_handler=router, max_requests_per_crawl=args.max_requests
     )
-
-    await crawler.run(["https://freemusicarchive.org"])
-    await crawler.export_data("results.json")
-    process("results.json")
+    print(args.urls)
+    await crawler.run(args.urls)
+    await crawler.export_data(args.output)
+    process(args.output)
 
 
 if __name__ == "__main__":
