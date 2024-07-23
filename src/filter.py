af7a6cf385dc7686f6f74abbae1feb6be58e086e
jourdelune <jourdelune863@gmail.com>
2024-07-23 20:58:43 +0200

    [update] use cachetools

--- src/filter.py
+++ src/filter.py
@@ -3,27 +3,30 @@ Filter class
 """
 
 import urllib.parse
+from cachetools import TTLCache
 
 
 class Filter:
     """
-    Class to filter the domain and avoid to crawl the same domain if it does not contain any song
+    Class to filter the domain and avoid crawling the same domain if it does not contain any song
     """
 
-    def __init__(self, max_crawl: int = 20, cache_size: int = 500) -> None:
+    def __init__(
+        self, max_crawl: int = 20, cache_size: int = 500, ttl: int = 600
+    ) -> None:
         """Class to filter the domain
 
         Args:
             max_crawl (int, optional): the maximum number of crawl before reject
             the url if no song is added. Defaults to 20.
             cache_size (int, optional): the cache size. Defaults to 500.
+            ttl (int, optional): time to live for cache items in seconds. Defaults to 600 (10 minutes).
         """
-        self._domains = {}
-        self._cache_size = cache_size
         self._max_crawl = max_crawl
+        self._domains = TTLCache(maxsize=cache_size, ttl=ttl)
 
     def check_domain(self, url: str) -> bool:
-        """Check if the domain is has not been visited more than max_crawl times and not songs are added
+        """Check if the domain has not been visited more than max_crawl times and no songs are added
 
         Args:
             url (str): the url to check
@@ -31,12 +34,7 @@ class Filter:
         Returns:
             bool: True if the domain has not been visited more than max_crawl times, False otherwise
         """
-
         domain = urllib.parse.urlparse(url).netloc
-
-        if len(self._domains) > self._cache_size:
-            oldest_domain = list(self._domains.keys())[0]
-            del self._domains[oldest_domain]
 
         if domain not in self._domains:
             self._domains[domain] = 1
@@ -51,6 +49,5 @@ class Filter:
         Args:
             url (str): the url to check
         """
-
         domain = urllib.parse.urlparse(url).netloc
         self._domains[domain] = 0
